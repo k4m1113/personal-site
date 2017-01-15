4d7386dd4a8a7611b8cd2f87ac5fff938514a980
Kamille Delgardo <kamilledelgardo@gmail.com>
2017-01-15 10:26:00 -0800

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,14 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader', 
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
     ]
   }
 }
--- webpack.server.config.js
+++ webpack.server.config.js
@@ -26,7 +26,14 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
     ]
   }
 
